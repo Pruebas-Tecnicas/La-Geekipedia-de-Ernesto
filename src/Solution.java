cc5dc76d129c2f9e2153821a33dd892b2486784e
Arath Medina <arath.mm19@gmail.com>
2022-09-16 01:42:24 -0500

    Solución v2 - Paréntesis válidos

--- src/Solution.java
+++ src/Solution.java
@@ -1,34 +1,44 @@
-
-public class Solution {
+/**
+ * 
+ * @author antonio
+ * 
+ */
+class Solution {
 	public boolean isValid(String s) {
-		s = s.replace(" ", "");
 		int i = 1;
+		boolean flag;
+
+		s = s.replace(" ", "");
 
 		if (s.length() % 2 != 0) {
 			return false;
 		}
 
 		do {
+			flag = false;
+
 			try {
 				String s1 = s.substring(i - 1, i);
 				String s2 = s.substring(i, i + 1);
 
 				if (s1.equals("(") && s2.equals(")")) {
-					s = s.substring(0, i - 1) + s.substring(i + 1, s.length());
-					i = 1;
+					flag = true;
 
 				} else if (s1.equals("[") && s2.equals("]")) {
-					s = s.substring(0, i - 1) + s.substring(i + 1, s.length());
-					i = 1;
+					flag = true;
 
 				} else if (s1.equals("{") && s2.equals("}")) {
+					flag = true;
+
+				}
+
+				if (flag) {
 					s = s.substring(0, i - 1) + s.substring(i + 1, s.length());
 					i = 1;
-
 				} else {
 					i++;
 				}
-				
+
 			} catch (Exception e) {
 				return false;
 			}
